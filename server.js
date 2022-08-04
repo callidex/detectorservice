37195fd54ad282446472374e5f83d5ae40586fbb
callidex <paul@callidex.com>
2022-08-04 21:20:51 +1000

    Write status and sample to db

--- server.js
+++ server.js
@@ -16,13 +16,11 @@ server.on('error', (err) => {
 server.on('message', (msg, rinfo) => {
 
   const data = Buffer.from(msg);
-  console.log(data);
   console.log(data.length);
   var packetNumber = data.readInt32LE(0) & 0xFFFFFF;
-  console.log("Packet number ", packetNumber);
+  var tempObject = new Object;
   if (data[3] == 0) {
     console.log("Sample packet incoming");
-    var tempObject = new Object;
     tempObject.udpnumber = data.readUInt32LE(0);
     tempObject.adcseq = data[4];
     tempObject.detector = data[6];
@@ -35,33 +33,36 @@ server.on('message', (msg, rinfo) => {
       tempObject.data.push((data[i + 15] << 8) + data[i + 1 + 15]);
     }
     tempObject.needsprocessing = 1;
-
     storeSample(tempObject);
 
   }
-  else if (data[3] == 1) {
+  else if (data[3] == 1 || data[3] == 2) {
     console.log("Status packet incoming");
+    tempObject.clocktrim = data.readUInt32LE(88);
+    tempObject.detectoruid = data.readUInt32LE(92) & 0x3FFFF;
+    tempObject.packetssent = data.readUInt32LE(96);
+    tempObject.triggeroffset = data.readUInt16LE(100);
+    tempObject.triggernoise = data.readUInt16LE(102);
+    tempObject.sysuptime = data.readUInt32LE(104);
+    tempObject.netuptime = data.readUInt32LE(108);
+    tempObject.gpsuptime = data.readUInt32LE(112);
+    tempObject.majorversion = data[116];
+    tempObject.minorversion = data[117];
+    tempObject.avgadcnoise = data.readUInt16LE(118);
+    tempObject.batchid = data[120];
+    tempObject.address = rinfo.address;
 
-    // don't mess about, store it now
-    /*
-        var packet = {
-          version: "0.1",
-          type: "data",
-          data: message,
-          address: rinfo.address,
-          port: rinfo.port,
-          timestamp: Date.now(),
-          processed: 0,
-    
-    
-        };
-    
-        storeRawData();
-    */
+    if (tempObject.detectoruid > 1000) {
+      console.log("incorrect status, dropping");
+      console.log("server got msg from ${data.readUInt32LE(92) & 0x3FFFF}:${data.readUInt32LE(92)}");
+      return null;
+    }
+
+    storeStatus(tempObject);
 
   }
 
-  console.log(`server got: ${msg} from ${rinfo.address}:${rinfo.port}`);
+  console.log(`server got msg from ${rinfo.address}:${rinfo.port}`);
 });
 
 server.on('listening', () => {
@@ -83,14 +84,43 @@ const pool = new Pool({
   connectionTimeoutMillis: 2000,
 });
 
+async function storeStatus(parsedObject) {
+  const client = await pool.connect();
+  try {
+    var sql = "insert into status (address, avgadcnoise, batchid, clocktrim, detectoruid,majorversion, minorversion, netuptime,  packettype, sysuptime, triggernoise, triggeroffset)" +
+      "VALUES ('" + parsedObject.address + "'," + parsedObject.avgadcnoise
+      + "," + parsedObject.batchid + ","
+      + parsedObject.clocktrim + ","
+      + parsedObject.detectoruid + ","
+      + parsedObject.majorversion + ","
+      + parsedObject.minorversion + ","
+      + parsedObject.netuptime + ","
+      + parsedObject.packetssent + ","
+      + parsedObject.sysuptime + ","
+      + parsedObject.triggernoise + ","
+      + parsedObject.triggeroffset + ")";
+    //    + parsedObject.packetnumber + ","
+    //    + parsedObject.packettype + ","
+    //   + parsedObject.received + ","
+
+
+    await client.query(sql);
+  }
+  catch (err) {
+    console.log(err.stack)
+    console.log(sql);
+  }
+
+  finally {
+    client.release();
+  }
+}
+
 async function storeSample(tempObject) {
   const client = await pool.connect();
   try {
     console.log(tempObject.detector);
-    const res = await client.query("INSERT INTO sample (detector) VALUES ($1) RETURNING *",
-      values = [tempObject.detector]
-    );
-
+    await client.query("INSERT INTO sample (detector, data, dmatime, batchid,rtsecs,adcseq) VALUES ($1, $2, $3, $4, $5, $6)", [tempObject.detector, tempObject.data, tempObject.dmatime, tempObject.batchid, tempObject.rtsecs, tempObject.adcseq]);
 
   }
   catch (err) { console.log(err.stack) }
@@ -104,60 +134,7 @@ async function storeSample(tempObject) {
 
 
 
-//pool.connect();
-
-
-//client.connect();
-//client.query('SELECT * from sample', (err, res) => { console.log(err.stack)});
-//client.query('SELECT * from sample').then(res => console.log(res)).catch(err => console.log(err));
-;
 
 
 
-// routes
-//router.post("/status", status);
-//router.post("/strike", strike);
-
-
-
-
-
-/*
-var storeDataPacket = function (parsedObject, con, rawpacketid) {
-  if (parsedObject.maxval < 4096) {
-    if (parsedObject.data != undefined && parsedObject.signal != undefined) {
-      var fixeddata = repairendian(parsedObject.data);
-      var fixedsignal = repairendian(parsedObject.signal);
-
-      var query = "INSERT INTO `datapackets` SET ?",
-        values = {
-          rawpacketid: rawpacketid,
-          adcseq: parsedObject.adcseq,
-          dmatime: parsedObject.dmatime,
-          maxval: parsedObject.maxval,
-          mean: parsedObject.mean,
-          needsprocessing: 1,
-          packetnumber: parsedObject.packetnumber,
-          packettype: parsedObject.packettype,
-          received: parsedObject.received,
-          rtsecs: parsedObject.rtsecs,
-          signaldata: new Buffer(fixedsignal),
-          signalcnt: 0,  //parsedObject.signalcnt,
-          stddev: parsedObject.stddev,
-          udpnumber: parsedObject.udpnumber,
-          variance: parsedObject.variance,
-          version: clean(parsedObject.version),
-          address: parsedObject.address,
-          batchid: parsedObject.batchid,
-          detectoruid: parsedObject.detectoruid,
-          data: new Buffer(fixeddata),
-        };
-      con.query(query, values, function (er, da) {
-        if (er) throw er;
-        backfilldatapacketfromstatus(con, parsedObject.batchid, parsedObject.detectoruid);
-      });
-    }
-  }
-}
-*/
 
